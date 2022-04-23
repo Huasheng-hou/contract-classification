4bb812ad94f10c680ce1210addacf159345e955b
HUASHENG <331292108@qq.com>
2022-04-23 20:35:06 +0800

    添加词性识别

--- preprocess.py
+++ preprocess.py
@@ -8,6 +8,21 @@ texts = np.array(corpus['正文'])
 
 ltp = LTP()
 seg, hidden = ltp.seg(list(texts)[:1])
+
+pos = ltp.pos(hidden)
 ner = ltp.ner(hidden)
+srl = ltp.srl(hidden)
+dep = ltp.dep(hidden)
+sdp = ltp.sdp(hidden)
 
 entities = [seg[0][n[2]] for n in ner[0]]
+npos, nouns = [], []
+
+for idx, p in enumerate(pos[0]):
+    if 'n' in p and p not in ['ng', 'nr', 'ns', 'nz', 'nh', 'nt']:
+    # if 'n' in p:
+        npos.append(p)
+        nouns.append(seg[0][idx])
+
+for idx, p in enumerate(npos):
+    print(p, nouns[idx])
