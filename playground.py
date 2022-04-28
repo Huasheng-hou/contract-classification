f42a2ed2324be38a38dd4a69169551871372df4a
HUASHENG <331292108@qq.com>
2022-04-28 11:45:10 +0800

    preprocess annotations

--- playground.py
+++ playground.py
@@ -1,24 +1,45 @@
 import pandas as pd
+import numpy as np
+
+from ltp import LTP
+
+ltp = LTP()
 
 f = pd.read_excel('data/contract_notation.xlsx')
-cls_idx = 0
-samples, classes = [], []
+samples = []
 
-for col in f.columns:
-    if 'Keywords' in col or 'Key words' in col:
-        cls_idx += 1
-        print(col)
-        words = f[col].dropna()
-        print(words)
-        for w in words:
-            if w == '/':
+for idx, row in f.iterrows():
+
+    cls_idx = 0
+    text = row['Contract']
+    seg, hidden = ltp.seg([text])
+    seg = seg[0]
+    locs = []
+    start = 0
+    classes = np.zeros(len(seg))
+    for s in seg:
+        locs.append(start)
+        start += len(s)
+
+    for col, item in row.iteritems():
+        if 'Keywords' in col or 'Key words' in col:
+            cls_idx += 1
+            word = item
+            if not isinstance(word, str) or word == '/':
                 continue
-            samples.append(w)
-            classes.append(cls_idx)
+            print(word)
+            loc = text.find(word)
+            print(loc)
+            if loc != -1 and loc in locs:
+                s_loc = locs.index(loc)
+                classes[s_loc] = cls_idx
+
+    samples.append(','.join(seg))
+    samples.append(','.join(list(classes.astype(int).astype(str))))
+    print('====================================')
 
 data = pd.DataFrame({
-    'keyword': samples,
-    'class': classes,
+    'samples': samples,
 })
 
 data.to_csv('data/samples.csv', index=False)
